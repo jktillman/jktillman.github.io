c39c2b1ff595c471c8581ec3a936cc7db0a94d56
jktillman <jt8685@live.missouristate.edu>
2017-03-29 12:21:18 -0500

    added .js , time, and verification of form

---
+++ myscript.js
@@ -0,0 +1,18 @@
+
+
+
+
+var d = new Date();
+document.getElementById("date").innerHTML = d;
+
+function validateForm() {
+    var a = document.forms["myForm"]["fname"].value;
+    var b= document.forms["myForm"]["lname"].value;
+	var c = document.forms["myForm"]["email"].value;
+	
+	if (a==null || a=="" || b==null || b=="" || c==null || c=="")
+      {
+      alert("Please Complete All Fields");
+      return false;
+      }
+    }
